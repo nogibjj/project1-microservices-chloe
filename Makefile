44a1a2790b1b35bc0e6e4eea87f2daba7460f6f9
chloekang234 <104106034+chloekang234@users.noreply.github.com>
2023-02-07 18:42:52 +0000

    update test_app

--- Makefile
+++ Makefile
@@ -4,9 +4,6 @@ install:
 
 format:
 		black *.py
-		
-lint:
-	pylint --disable=R,C app.py
 
 test:
 	python -m pytest -vv --cov=app test_app.py
--- app.py
+++ app.py
@@ -3,39 +3,44 @@ from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///todo.db'
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///todo.db"
 
 db = SQLAlchemy(app)
 app.app_context().push()
+
 
 class Todo(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     text = db.Column(db.String(200))
     complete = db.Column(db.Boolean)
 
-@app.route('/')
+
+@app.route("/")
 def index():
     incomplete = Todo.query.filter_by(complete=False).all()
     complete = Todo.query.filter_by(complete=True).all()
-    return render_template('index.html', incomplete=incomplete, complete=complete)
+    return render_template("index.html", incomplete=incomplete, complete=complete)
 
-@app.route('/add', methods=['POST'])
+
+@app.route("/add", methods=["POST"])
 def add():
     # return '<h1>{}</h1>'.format(request.form['todoitem'])
-    todo = Todo(text=request.form['todoitem'], complete=False)
+    todo = Todo(text=request.form["todoitem"], complete=False)
     db.session.add(todo)
     db.session.commit()
-    return redirect(url_for('index'))
+    return redirect(url_for("index"))
 
-@app.route('/complete/<id>')
+
+@app.route("/complete/<id>")
 def complete(id):
     todo = Todo.query.filter_by(id=int(id)).first()
     todo.complete = True
     db.session.commit()
 
-    return redirect(url_for('index'))
+    return redirect(url_for("index"))
 
-@app.route('/delete/<id>')
+
+@app.route("/delete/<id>")
 def delete(id):
     # if it does not exist, return 404
     task_to_delete = Todo.query.get_or_404(id)
@@ -44,9 +49,10 @@ def delete(id):
         db.session.delete(task_to_delete)
         db.session.commit()
         # redirect to home page
-        return redirect('/')
+        return redirect("/")
     except:
-        return 'having trouble deleting task'
+        return "having trouble deleting task"
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     app.run(debug=True)
